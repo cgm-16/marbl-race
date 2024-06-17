03d6e8f62f15e1e53362077b9e86c357dd47995e
Gwangmin Choi <crazy4u5@naver.com>
2024-06-17 22:01:03 +0900

    [DEV] marble + slide fiddling

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,102 @@
 <script setup lang="ts">
+import { ref, onMounted } from "vue";
+import * as THREE from "three";
 
-import { ref, onMounted } from 'vue';
-import * as THREE from 'three';
+import { GLTFLoader } from "three/examples/jsm/Addons.js";
+import { Octree } from "three/examples/jsm/Addons.js";
+import { randFloat } from "three/src/math/MathUtils.js";
+import { OrbitControls } from "three/examples/jsm/Addons.js";
+
+const clock = new THREE.Clock();
 
 const target = ref();
 
+const colors = [0x6488ea, 0x0d2464, 0x6b8deb, 0xff460e, 0xa6ff70]
+
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setAnimationLoop(animate);
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.VSMShadowMap;
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshPhongMaterial({
-  color: 0x555555,
-  specular: 0xffffff,
-  shininess: 50
-});
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const fillLight1 = new THREE.HemisphereLight(0x8dc1de, 0x00668d, 1.5);
+fillLight1.position.set(2, 1, 1);
+scene.add(fillLight1);
 
-const light = new THREE.DirectionalLight( 0xffffff );
-light.position.set(0, 1, 1);
-scene.add(light)
+const directionalLight = new THREE.DirectionalLight(0xffffff, 2.5);
+directionalLight.position.set(-5, 25, -1);
+directionalLight.castShadow = true;
+directionalLight.shadow.camera.near = 0.01;
+directionalLight.shadow.camera.far = 500;
+directionalLight.shadow.camera.right = 30;
+directionalLight.shadow.camera.left = -30;
+directionalLight.shadow.camera.top = 30;
+directionalLight.shadow.camera.bottom = -30;
+directionalLight.shadow.mapSize.width = 1024;
+directionalLight.shadow.mapSize.height = 1024;
+directionalLight.shadow.radius = 4;
+directionalLight.shadow.bias = -0.00006;
+scene.add(directionalLight);
 
-camera.position.z = 5;
+camera.position.z = 70;
+camera.position.y = 70;
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.minDistance = 2;
+controls.maxDistance = 10;
+controls.target.set(0, 60, 40);
+controls.update();
+
+window.addEventListener("resize", onWindowResize);
+
+const GRAVITY = 30;
+
+const NUM_SPHERES = 10;
+const SPHERE_RADIUS = 0.2;
+
+const STEPS_PER_FRAME = 5;
+
+const sphereGeometry = new THREE.IcosahedronGeometry(SPHERE_RADIUS, 5);
+
+const spheres: any[] = [];
+
+for (let i = 0; i < NUM_SPHERES; i++) {
+  const sphereMaterial = new THREE.MeshLambertMaterial({ color: colors[~~(Math.random() * (colors.length+1))] });
+  const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+  sphere.castShadow = true;
+  sphere.receiveShadow = true;
+
+  scene.add(sphere);
+
+  spheres.push({
+    mesh: sphere,
+    collider: new THREE.Sphere(
+      new THREE.Vector3(
+        randFloat(-10, 10),
+        randFloat(70, 80),
+        randFloat(-10, 10)
+      ),
+      SPHERE_RADIUS
+    ),
+    velocity: new THREE.Vector3(),
+  });
+}
+
+const worldOctree = new Octree();
+
+const vector1 = new THREE.Vector3();
+const vector2 = new THREE.Vector3();
+const vector3 = new THREE.Vector3();
 
 window.addEventListener("resize", onWindowResize);
 
@@ -36,11 +107,88 @@ function onWindowResize() {
   renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
-function animate() {
-  requestAnimationFrame(animate);
+const loader = new GLTFLoader();
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+loader.load("slide.glb", (gltf) => {
+  scene.add(gltf.scene);
+
+  worldOctree.fromGraphNode(gltf.scene);
+
+  gltf.scene.traverse((child) => {
+    if (child instanceof THREE.Mesh) {
+      child.castShadow = true;
+      child.receiveShadow = true;
+
+      if (child.material.map) {
+        child.material.map.anisotropy = 4;
+      }
+    }
+  });
+});
+
+function spheresCollisions() {
+  for (let i = 0, length = spheres.length; i < length; i++) {
+    const s1 = spheres[i];
+
+    for (let j = i + 1; j < length; j++) {
+      const s2 = spheres[j];
+
+      const d2 = s1.collider.center.distanceToSquared(s2.collider.center);
+      const r = s1.collider.radius + s2.collider.radius;
+      const r2 = r * r;
+
+      if (d2 < r2) {
+        const normal = vector1
+          .subVectors(s1.collider.center, s2.collider.center)
+          .normalize();
+        const v1 = vector2.copy(normal).multiplyScalar(normal.dot(s1.velocity));
+        const v2 = vector3.copy(normal).multiplyScalar(normal.dot(s2.velocity));
+
+        s1.velocity.add(v2).sub(v1);
+        s2.velocity.add(v1).sub(v2);
+
+        const d = (r - Math.sqrt(d2)) / 2;
+
+        s1.collider.center.addScaledVector(normal, d);
+        s2.collider.center.addScaledVector(normal, -d);
+      }
+    }
+  }
+}
+
+function updateSpheres(deltaTime: number) {
+  spheres.forEach((sphere) => {
+    sphere.collider.center.addScaledVector(sphere.velocity, deltaTime);
+
+    const result = worldOctree.sphereIntersect(sphere.collider);
+
+    if (result) {
+      sphere.velocity.addScaledVector(
+        result.normal,
+        -result.normal.dot(sphere.velocity) * 1.5
+      );
+      sphere.collider.center.add(result.normal.multiplyScalar(result.depth));
+    } else {
+      sphere.velocity.y -= GRAVITY * deltaTime;
+    }
+
+    const damping = Math.exp(-1.5 * deltaTime) - 1;
+    sphere.velocity.addScaledVector(sphere.velocity, damping);
+  });
+
+  spheresCollisions();
+
+  for (const sphere of spheres) {
+    sphere.mesh.position.copy(sphere.collider.center);
+  }
+}
+
+function animate() {
+  const deltaTime = Math.min(0.05, clock.getDelta()) / STEPS_PER_FRAME;
+
+  for (let i = 0; i < STEPS_PER_FRAME; i++) {
+    updateSpheres(deltaTime);
+  }
 
   renderer.render(scene, camera);
 }
@@ -57,13 +205,13 @@ onMounted(() => {
 </template>
 
 <style scoped>
-  .TODO {
-    --width: 300px;
-    position: absolute;
-    width: var(--width);
-    top: 10vh;
-    left: calc(50vw - var(--width)/2);
-    color: azure;
-    text-align: center;
-  }
+.TODO {
+  --width: 300px;
+  position: absolute;
+  width: var(--width);
+  top: 10vh;
+  left: calc(50vw - var(--width) / 2);
+  color: azure;
+  text-align: center;
+}
 </style>
